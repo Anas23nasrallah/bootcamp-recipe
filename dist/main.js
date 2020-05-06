08827b109a051686b809e08501c730b4a4b256bb
AnasNasrallah <anas.23.nasrallah@gmail.com>
2020-05-06 11:41:42 +0300

    3rd commit

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,18 @@
+/*
+Authore: Anas Nasrallah.
+Purpose: Proficiency Exam.
+Date: 06.05.20
+*/
 
+$('#searchBtn').on('click', function () {
+    const inputIngredient = $('#ingredientInput').val()
+    $('#ingredientInput').val('')
+    $.get(`/recipes/${inputIngredient}`, function (recipes) {
+        console.log(recipes)
+        console.log(recipes[0].ingredients)
+        console.log(recipes[0].title)
+        console.log(recipes[0].thumbnail)
+        console.log(recipes[0].href)
 
-
-
-
-$.get('https://recipes-goodness.herokuapp.com/recipes/YOUR_INGREDIENT', function (data) {
-    console.log(data)
+    })
 })
