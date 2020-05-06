7bbf45a0eeb96ab5881f25a369437e61964b99cc
AnasNasrallah <anas.23.nasrallah@gmail.com>
2020-05-06 12:16:42 +0300

    4thd commit

--- dist/main.js
+++ dist/main.js
@@ -3,16 +3,33 @@ Authore: Anas Nasrallah.
 Purpose: Proficiency Exam.
 Date: 06.05.20
 */
+class Renderer {
+
+    renderOne(recipe) {
+        const source = $('#recipe-temp').html();
+        const template = Handlebars.compile(source)
+        let recipeHTML = template(recipe)
+        $('#ingredients').append(recipeHTML)
+    }
+
+    render(data){
+        $('#ingredients').empty()
+        data.forEach(recipe => this.renderOne(recipe))
+    }
+}
+
+const renderer = new Renderer()
 
 $('#searchBtn').on('click', function () {
     const inputIngredient = $('#ingredientInput').val()
     $('#ingredientInput').val('')
     $.get(`/recipes/${inputIngredient}`, function (recipes) {
         console.log(recipes)
-        console.log(recipes[0].ingredients)
-        console.log(recipes[0].title)
-        console.log(recipes[0].thumbnail)
-        console.log(recipes[0].href)
-
+        renderer.render(recipes.map(recipe => ({
+            title: recipe.title,
+            thumbnail: recipe.thumbnail,
+            ingredients: recipe.ingredients,
+            href: recipe.href
+        })))
     })
 })
