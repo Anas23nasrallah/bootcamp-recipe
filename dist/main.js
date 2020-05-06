86aa2728d210bfa8482fc9de29120f5a3faf9433
AnasNasrallah <anas.23.nasrallah@gmail.com>
2020-05-06 13:50:22 +0300

    last commit- sorry for the styling :(

--- dist/main.js
+++ dist/main.js
@@ -3,17 +3,18 @@ Authore: Anas Nasrallah.
 Purpose: Proficiency Exam.
 Date: 06.05.20
 */
+
 class Renderer {
 
     renderOne(recipe) {
         const source = $('#recipe-temp').html();
         const template = Handlebars.compile(source)
         let recipeHTML = template(recipe)
-        $('#ingredients').append(recipeHTML)
+        $('#recipes').append(recipeHTML)
     }
 
     render(data){
-        $('#ingredients').empty()
+        $('#recipes').empty()
         data.forEach(recipe => this.renderOne(recipe))
     }
 }
@@ -24,7 +25,6 @@ $('#searchBtn').on('click', function () {
     const inputIngredient = $('#ingredientInput').val()
     $('#ingredientInput').val('')
     $.get(`/recipes/${inputIngredient}`, function (recipes) {
-        console.log(recipes)
         renderer.render(recipes.map(recipe => ({
             title: recipe.title,
             thumbnail: recipe.thumbnail,
@@ -32,4 +32,9 @@ $('#searchBtn').on('click', function () {
             href: recipe.href
         })))
     })
+})
+
+$('#recipes').on('click', 'img', function(){
+    const $firstIngredient = $(this).siblings('ul').find('li').first()
+    console.log($firstIngredient.text())
 })
